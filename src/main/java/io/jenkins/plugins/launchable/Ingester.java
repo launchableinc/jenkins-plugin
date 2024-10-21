9c4f42bb581abe3aebde993cd87cf43d3b843de6
Kohsuke Kawaguchi <kk@kohsuke.org>
2024-10-21 14:39:26 +0900

    Implemented basic MIME multipart content submission

--- src/main/java/io/jenkins/plugins/launchable/Ingester.java
+++ src/main/java/io/jenkins/plugins/launchable/Ingester.java
@@ -1,15 +1,27 @@
 package io.jenkins.plugins.launchable;
 
 import hudson.Extension;
+import hudson.model.Descriptor;
 import hudson.tasks.junit.TestResult;
 import hudson.util.Secret;
 import hudson.util.TextFile;
 import jenkins.model.GlobalConfiguration;
 import net.sf.json.JSONObject;
+import org.apache.http.HttpEntity;
+import org.apache.http.client.entity.GzipCompressingEntity;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.ContentType;
+import org.apache.http.entity.mime.MultipartEntityBuilder;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
 import org.kohsuke.stapler.StaplerRequest;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 @Extension
 public class Ingester extends GlobalConfiguration {
@@ -30,17 +42,40 @@ public class Ingester extends GlobalConfiguration {
     }
 
     /**
-     * @param dir
-     *      Directory that's supposed to contain test results. See {@link TestResult} for the data format.
-     * @param properties
-     *      Additional contextual data to submit along with the test results.
+     * @param dir        Directory that's supposed to contain test results. See {@link TestResult} for the data format.
+     * @param properties Additional contextual data to submit along with the test results.
      */
     /*package*/ void slurp(File dir, PropsBuilder<?> properties) throws IOException {
-        File report = new File(dir,"junitResult.xml");
-        if (!report.exists())       return; // be defensive just in case
+        File report = new File(dir, "junitResult.xml");
+        if (!report.exists()) return; // be defensive just in case
 
-        System.out.println("Sending "+report+" to Launchable");
-        System.out.println(properties.build().toString());
-        System.out.println(new TextFile(report).read());
+        // attempted to use JDK HttpRequest, but gave up due to the lack of multipart support
+        // TODO: how do I obtain a properly configured HttpClient for the proxy setting in Jenkins?
+        try (CloseableHttpClient httpClient = HttpClients.createDefault()) {
+
+            String endpoint = System.getenv("INSIGHT_UPLOAD_URL");
+            if (endpoint==null) {
+                endpoint = DEFAULT_UPLOAD_URL;
+            }
+            var hc = new HttpPost(endpoint);
+
+            var builder = MultipartEntityBuilder.create();
+            builder.addTextBody("metadata", properties.build().toString(), ContentType.APPLICATION_JSON);
+            builder.addBinaryBody("file", report, ContentType.APPLICATION_XML, "junitResult.xml");
+
+            hc.setEntity(new GzipCompressingEntity(builder.build()));
+            hc.addHeader("Authorization", "Bearer " + apiKey.getPlainText());
+
+            try (CloseableHttpResponse response = httpClient.execute(hc)) {
+                if (response.getStatusLine().getStatusCode() >= 300) {
+                    // treat redirect as an error, for the time being. we submit a big payload, so we don't want
+                    // to be forced to repeat the payload after we send the whole thing once.
+                    LOGGER.log(Level.WARNING, "Failed to submit test results: {0}", response.getStatusLine());
+                }
+            }
+        }
     }
+
+    private static final Logger LOGGER = Logger.getLogger(Ingester.class.getName());
+    private static final String DEFAULT_UPLOAD_URL = "https://api.mercury.launchableinc.com/TODO";
 }
