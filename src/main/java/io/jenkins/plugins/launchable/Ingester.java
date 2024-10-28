b06f89d6b829737eeae41b91ebf1299161868081
Kohsuke Kawaguchi <kk@kohsuke.org>
2024-10-28 16:02:23 -0700

    Redundant call
    Apache HttpClient is designed to allow entity to report Content-Encoding header on its own, so no need to set this by ourselves. It leaks abstractions.

--- src/main/java/io/jenkins/plugins/launchable/Ingester.java
+++ src/main/java/io/jenkins/plugins/launchable/Ingester.java
@@ -67,7 +67,6 @@ public class Ingester extends GlobalConfiguration {
 
                 hc.setEntity(new GzipCompressingEntity(builder.build()));
                 hc.addHeader("Authorization", "Bearer " + apiKey.getPlainText());
-                hc.setHeader(HttpHeaders.CONTENT_ENCODING, "gzip");
 
                 try (CloseableHttpResponse response = httpClient.execute(hc)) {
                     if (response.getStatusLine().getStatusCode() >= 300) {
